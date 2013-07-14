32b9c58fcf9f0e8474a2c050fb35c110f37a25bb
root <root@ip-10-252-45-155.us-west-2.compute.internal>
2013-07-14 11:13:37 +0000

    keep alive fixed

--- scripts/arduwebsocket.js
+++ scripts/arduwebsocket.js
@@ -5,7 +5,7 @@ var  clients = [];
 
 exports.Handler = function(p_server ){  
   
-  wsServer = new webSocketServer({httpServer:p_server, keepalive:true, keepaliveInterval: 10000});
+  wsServer = new webSocketServer({httpServer:p_server, keepalive:false});
   
   wsServer.on('request', function(request){
     
