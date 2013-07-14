2eb3191b5159f90d72bd493cdc68fb54d2f036c2
e.marin.izquierdo@gmail.com <>
2013-07-14 07:56:38 +0000

    arduwebsocket formatted

--- scripts/arduwebsocket.js
+++ scripts/arduwebsocket.js
@@ -1,25 +1,29 @@
-webSocketServer = require('websocket').server;
+
+var webSocketServer = require('websocket').server;
 
 exports.Handler = function(p_server ){
   
   var wsServer = new webSocketServer({httpServer:p_server});
-
+  
   wsServer.on('request', function(request){
-     console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
-     var connection = request.accept(null, request.origin);
-     console.log((new Date()) + ' Connection accepted.');
-
-     connection.on('message', function(message){
-        if (message.type === 'utf8') { // accept only text
-             	//get data object from message         
-		var data = message.utf8Data;
-        }
-      	console.log('message received: '+data);
-        connection.sendUTF(data);
-     });
-
-     connection.on('close', function(connection){
-     	console.log('connection closed');
-     });
+    
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+    var connection = request.accept(null, request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    
+    connection.on('message', function(message){
+      if (message.type === 'utf8') { // accept only text
+        //get data object from message         
+  		    var data = message.utf8Data;
+      }
+      console.log('message received: '+data);
+      connection.sendUTF(data);
+    });
+    
+    connection.on('close', function(connection){
+      console.log('connection closed');
+    });
+    
   });
 }
+  
